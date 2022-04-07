2eb3de86d0db99edef18eba2ac2dfea13617dea8
John Ouellet <john@thinktandem.io>
2022-04-06 21:07:32 -0400

    Adjust bash script

---
+++ scripts/functions.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+get_distribution() {
+	lsb_dist=""
+	# Every system that we officially support has /etc/os-release
+	if [ -r /etc/os-release ]; then
+		lsb_dist="$(. /etc/os-release && echo "$ID")"
+	fi
+	# Returning an empty string here should be alright since the
+	# case statements don't act unless you provide an actual value
+	echo "$lsb_dist"
+}
+
+is_wsl() {
+	case "$(uname -r)" in
+	*microsoft* ) true ;; # WSL 2
+	*Microsoft* ) true ;; # WSL 1
+	* ) false;;
+	esac
+}
+
+
--- scripts/preflight-checks.sh
+++ scripts/preflight-checks.sh
@@ -3,6 +3,51 @@ set -e
 
 # This script is meant to install dependencies needed to install docker
 
-# install curl if needed
-set -- "curl"
-. ./scripts/install-package.sh
+# Loads our functions used in this script
+. ./scripts/functions.sh
+
+if is_wsl; then
+	echo
+	echo "WSL DETECTED: We recommend using Docker Desktop for Windows."
+	echo "Please get Docker Desktop from https://www.docker.com/products/docker-desktop"
+	echo
+	exit 2
+fi
+
+for PACKAGE in curl nano
+do
+  if command_exists ${PACKAGE}; then
+    echo "$PACKAGE is installed"
+  else
+    echo
+    echo "$PACKAGE is not installed, installing..."
+    echo
+
+    # perform some very rudimentary platform detection
+    lsb_dist=$( get_distribution )
+    lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+
+    # install package as need be
+    case "$lsb_dist" in
+      ubuntu|debian|raspbian|pop)
+        sudo apt install -y ${PACKAGE}
+      ;;
+
+      centos|fedora|rhel)
+        if [ "$lsb_dist" = "fedora" ]; then
+          sudo dnf install -y ${PACKAGE}
+        else
+          sudo yum install -y ${PACKAGE}
+        fi
+      ;;
+
+      sles)
+        sudo zypper install -y ${PACKAGE}
+      ;;
+
+      *)
+        echo "ERROR: Unsupported distribution '$lsb_dist'"
+      ;;
+    esac
+  fi
+done
